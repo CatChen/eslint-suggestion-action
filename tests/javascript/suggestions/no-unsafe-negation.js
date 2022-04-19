2a3f65b50310c2930384c0d0522dc5bb4e341835
Cat Chen <catchen@catchen.me>
2022-04-19 11:50:01 -0700

    Fixed prettified tests

--- tests/javascript/suggestions/no-unsafe-negation.js
+++ tests/javascript/suggestions/no-unsafe-negation.js
@@ -1,64 +1,81 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function incorrect() {
   /*eslint no-unsafe-negation: "error"*/
 
+  // eslint-disable-next-line no-undef
   if (!key in object) {
     // operator precedence makes it equivalent to (!key) in object
     // and type conversion makes it equivalent to (key ? "false" : "true") in object
   }
 
+  // eslint-disable-next-line no-undef
   if (!obj instanceof Ctor) {
     // operator precedence makes it equivalent to (!obj) instanceof Ctor
     // and it equivalent to always false since boolean values are not objects.
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function correct() {
   /*eslint no-unsafe-negation: "error"*/
 
+  // eslint-disable-next-line no-undef
   if (!(key in object)) {
     // key is not in object
   }
 
+  // eslint-disable-next-line no-undef
   if (!(obj instanceof Ctor)) {
     // obj is not an instance of Ctor
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function exception() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function correct() {
     /*eslint no-unsafe-negation: "error"*/
 
-    if (!foo in object) {
+    // eslint-disable-next-line no-undef
+    if ((!foo) in object) {
       // allowed, because the negation is explicitly wrapped in parentheses
       // it is equivalent to (foo ? "false" : "true") in object
       // this is allowed as an exception for rare situations when that is the intended meaning
     }
 
-    if ("" + !foo in object) {
+    // eslint-disable-next-line no-undef
+    if(("" + !foo) in object) {
       // you can also make the intention more explicit, with type conversion
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function incorrect() {
     /*eslint no-unsafe-negation: "error"*/
 
-    if (!foo in object) {
+    // eslint-disable-next-line no-undef
+    if (!(foo) in object) {
       // this is not an allowed exception
     }
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function enforceForOrderingRelationsTrue() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function incorrect() {
     /*eslint no-unsafe-negation: ["error", { "enforceForOrderingRelations": true }]*/
 
-    if (!a < b) {
-    }
+    // eslint-disable-next-line no-undef, no-empty
+    if (! a < b) {}
 
-    while (!a > b) {}
+    // eslint-disable-next-line no-undef, no-empty
+    while (! a > b) {}
 
-    foo = !a <= b;
+    // eslint-disable-next-line no-undef
+    foo = ! a <= b;
 
-    foo = !a >= b;
+    // eslint-disable-next-line no-undef
+    foo = ! a >= b;
   }
 }
