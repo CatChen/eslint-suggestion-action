7e1e78985d872449b2cb147436ac415874fc2617
Cat Chen <catchen@catchen.me>
2022-04-20 15:16:38 -0700

    Added 4 fix tests

---
+++ tests/javascript/fixes/eqeqeq.js
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef, no-empty */
+function incorrect() {
+  /*eslint eqeqeq: "error"*/
+
+  if (x == 42) { }
+
+  if ("" == text) { }
+
+  if (obj.getStuff() != undefined) { }
+}
+
+function always() {
+  function incorrect() {
+    /*eslint eqeqeq: ["error", "always"]*/
+
+    a == b
+    foo == true
+    bananas != 1
+    value == undefined
+    typeof foo == 'undefined'
+    'hello' != 'world'
+    0 == 0
+    true == true
+    foo == null
+
+  }
+
+  function correct() {
+    /*eslint eqeqeq: ["error", "always"]*/
+
+    a === b
+    foo === true
+    bananas !== 1
+    value === undefined
+    typeof foo === 'undefined'
+    'hello' !== 'world'
+    0 === 0
+    true === true
+    foo === null
+  }
+}
+
+function smart() {
+  function incorrect() {
+    /*eslint eqeqeq: ["error", "smart"]*/
+
+    // comparing two variables requires ===
+    a == b
+
+    // only one side is a literal
+    foo == true
+    bananas != 1
+
+    // comparing to undefined requires ===
+    value == undefined
+  }
+
+  function correct() {
+    /*eslint eqeqeq: ["error", "smart"]*/
+
+    typeof foo == 'undefined'
+    'hello' != 'world'
+    0 == 0
+    true == true
+    foo == null
+  }
+}
