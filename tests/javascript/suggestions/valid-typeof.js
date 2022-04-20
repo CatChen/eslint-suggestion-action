fd66104c53465a0bd783529e343263064192dae2
Cat Chen <catchen@catchen.me>
2022-04-19 22:21:18 -0700

    Disabled irrelevant lint rules

--- tests/javascript/suggestions/valid-typeof.js
+++ tests/javascript/suggestions/valid-typeof.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef */
 function incorrect() {
   /*eslint valid-typeof: "error"*/
 
@@ -8,20 +8,16 @@ function incorrect() {
   typeof bar !== "fucntion"
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function correct() {
   /*eslint valid-typeof: "error"*/
 
   typeof foo === "string"
   typeof bar == "undefined"
-  // eslint-disable-next-line no-undef
   typeof foo === baz
   typeof bar === typeof qux
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function requireStringLiteralsTrue() {
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
   function incorrect() {
     /*eslint valid-typeof: ["error", { "requireStringLiterals": true }]*/
 
@@ -29,12 +25,10 @@ function requireStringLiteralsTrue() {
     typeof bar == Object
     typeof baz === "strnig"
     typeof qux === "some invalid type"
-    // eslint-disable-next-line no-undef
     typeof baz === anotherVariable
     typeof foo == 5
   }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
   function correct() {
     /*eslint valid-typeof: ["error", { "requireStringLiterals": true }]*/
 
