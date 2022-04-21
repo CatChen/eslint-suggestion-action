0aa8356da73d468520b166a2e1be6a7e44c78640
Cat Chen <catchen@catchen.me>
2022-04-21 16:48:31 -0700

    Separating tests of different configs

--- tests/javascript/fixes/dot-notation.js
+++ tests/javascript/fixes/dot-notation.js
@@ -12,28 +12,3 @@ function correct() {
 
     var x = foo[bar];    // Property name is a variable, square-bracket notation required
 }
-
-function allowKeywordsFalse() {
-  function correct() {
-    /*eslint dot-notation: ["error", { "allowKeywords": false }]*/
-
-    var foo = { "class": "CS 101" }
-    var x = foo["class"]; // Property name is a reserved word, square-bracket notation required
-  }
-}
-
-function allowPattern() {
-  function correct() {
-    /*eslint camelcase: "error"*/
-    /*eslint dot-notation: ["error", { "allowPattern": "^[a-z]+(_[a-z]+)+$" }]*/
-
-    var data = {};
-    data.foo_bar = 42;
-
-    var data = {};
-    data["fooBar"] = 42;
-
-    var data = {};
-    data["foo_bar"] = 42; // no warning
-  }  
-}
