8d09fda9e37953aaf28c3efdf6c0c7c5c89d097d
Cat Chen <catchen@catchen.me>
2022-04-21 20:20:19 -0700

    Added more tests for fixes

---
+++ tests/javascript/fixes/sort-imports-allowSeparatedGroupsTrue-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: ["error", { "allowSeparatedGroups": true }]*/
+
+import b from 'foo.js';
+import c from 'bar.js';
+import a from 'baz.js';
---
+++ tests/javascript/fixes/sort-imports-ignoreMemberSortTrue-correct.js
@@ -0,0 +1,3 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: ["error", { "ignoreMemberSort": true }]*/
+import {b, a, c} from 'foo.js'
