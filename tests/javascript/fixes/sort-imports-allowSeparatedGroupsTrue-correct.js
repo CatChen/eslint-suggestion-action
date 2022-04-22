88646a31dd95b25e5e35dec085afc91550ee1eb6
Cat Chen <catchen@catchen.me>
2022-04-21 21:52:45 -0700

    sort-imports

---
+++ tests/javascript/fixes/sort-imports-allowSeparatedGroupsTrue-correct.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef */
+/*eslint sort-imports: ["error", { "allowSeparatedGroups": true }]*/
+
+import b from 'foo.js';
+import c from 'bar.js';
+quux();
+import a from 'baz.js';
