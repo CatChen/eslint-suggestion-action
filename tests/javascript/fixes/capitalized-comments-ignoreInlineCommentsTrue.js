d9863fbce74c88d754e6fd98988ceb0a910b3bd3
Cat Chen <catchen@catchen.me>
2022-04-21 21:35:31 -0700

    capitalized-comments

---
+++ tests/javascript/fixes/capitalized-comments-ignoreInlineCommentsTrue.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+function correct() {
+  /* eslint capitalized-comments: ["error", "always", { "ignoreInlineComments": true }] */
+
+  function foo(/* ignored */ a) {
+  }
+}
