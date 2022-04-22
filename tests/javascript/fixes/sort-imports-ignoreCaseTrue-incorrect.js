0f06a2c12c5a30282d28e6ccf413866162948e2d
Cat Chen <catchen@catchen.me>
2022-04-21 20:20:19 -0700

    Added more tests for fixes

---
+++ tests/javascript/fixes/sort-imports-ignoreCaseTrue-incorrect.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: ["error", { "ignoreCase": true }]*/
+
+import B from 'foo.js';
+import a from 'bar.js';
---
+++ tests/javascript/fixes/sort-imports-memberSyntaxSortOrder-correct.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: ["error", { "memberSyntaxSortOrder": ['all', 'single', 'multiple', 'none'] }]*/
+
+import * as foo from 'foo.js';
+import z from 'zoo.js';
+import {a, b} from 'foo.js';
