e78bd30cdc5a6bf28b682600e299badd87b04cac
Cat Chen <catchen@catchen.me>
2022-04-21 21:49:41 -0700

    operator-assignment

---
+++ tests/javascript/fixes/operator-assignment-never.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef */
+function incorrect() {
+  /*eslint operator-assignment: ["error", "never"]*/
+
+  x *= y;
+  x ^= (y + z) / foo();
+}
+
+function correct() {
+  /*eslint operator-assignment: ["error", "never"]*/
+
+  x = x + y;
+  x.y = x.y / a.b;
+}
