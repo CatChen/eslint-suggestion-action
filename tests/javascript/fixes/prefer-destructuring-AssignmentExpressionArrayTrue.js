d6a8138e3d977d77c23a440ad2f0da424080c2f7
Cat Chen <catchen@catchen.me>
2022-04-21 21:50:28 -0700

    prefer-destructuring

---
+++ tests/javascript/fixes/prefer-destructuring-AssignmentExpressionArrayTrue.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef, no-redeclare */
+function correct() {
+  /* eslint prefer-destructuring: ["error", {"AssignmentExpression": {"array": true}}] */
+  [bar] = array;
+}
