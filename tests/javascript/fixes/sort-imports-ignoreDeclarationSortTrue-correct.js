53ed15a50f72f7c1541598da899540b7ebf40702
Cat Chen <catchen@catchen.me>
2022-04-21 21:52:45 -0700

    sort-imports

---
+++ tests/javascript/fixes/sort-imports-ignoreDeclarationSortTrue-correct.js
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: ["error", { "ignoreDeclarationSort": true }]*/
+import a from 'foo.js'
+import b from 'bar.js'
---
+++ tests/javascript/fixes/sort-imports-incorrect.js
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint sort-imports: "error"*/
+import b from 'foo.js';
+import a from 'bar.js';
