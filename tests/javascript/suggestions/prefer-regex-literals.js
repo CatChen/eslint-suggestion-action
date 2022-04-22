d95a1070cb4d44e1412324fb62bdf42c2d3d2cb8
Cat Chen <catchen@catchen.me>
2022-04-21 17:07:29 -0700

    Separating tests of different configs

--- tests/javascript/suggestions/prefer-regex-literals.js
+++ tests/javascript/suggestions/prefer-regex-literals.js
@@ -40,23 +40,3 @@ function correct() {
 
   new RegExp(String.raw`^\d\. ${suffix}`);
 }
-
-function disallowRedundantWrappingTrue() {
-  function incorrect() {
-    /*eslint prefer-regex-literals: ["error", {"disallowRedundantWrapping": true}]*/
-
-    new RegExp(/abc/);
-
-    new RegExp(/abc/, 'u');
-  }
-  
-  function correct() {
-    /*eslint prefer-regex-literals: ["error", {"disallowRedundantWrapping": true}]*/
-
-    /abc/;
-
-    /abc/u;
-
-    new RegExp(/abc/, flags);
-  }  
-}
