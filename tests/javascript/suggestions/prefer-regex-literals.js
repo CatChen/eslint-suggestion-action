9e1497e67c7b5f85feb0ce4de99887c0f4f1ea9c
Cat Chen <catchen@catchen.me>
2022-04-21 20:45:09 -0700

    prefer-regex-literals

---
+++ tests/javascript/suggestions/prefer-regex-literals.js
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef */
+function incorrect() {
+  /*eslint prefer-regex-literals: "error"*/
+
+  new RegExp("abc");
+
+  new RegExp("abc", "u");
+
+  RegExp("abc");
+
+  RegExp("abc", "u");
+
+  new RegExp("\\d\\d\\.\\d\\d\\.\\d\\d\\d\\d");
+
+  RegExp(`^\\d\\.$`);
+
+  new RegExp(String.raw`^\d\.$`);
+}
+
+function correct() {
+  /*eslint prefer-regex-literals: "error"*/
+
+  /abc/;
+
+  /abc/u;
+
+  /\d\d\.\d\d\.\d\d\d\d/;
+
+  /^\d\.$/;
+
+  // RegExp constructor is allowed for dynamically generated regular expressions
+
+  new RegExp(pattern);
+
+  RegExp("abc", flags);
+
+  new RegExp(prefix + "abc");
+
+  RegExp(`${prefix}abc`);
+
+  new RegExp(String.raw`^\d\. ${suffix}`);
+}
