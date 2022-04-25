d80d5b3ceb5f01d582a8d9acca4bc3175f245846
Cat Chen <catchen@catchen.me>
2022-04-24 21:46:23 -0700

    Added layout fix tests

---
+++ tests/javascript/fixes/array-element-newline-consistent.js
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, no-undef, no-redeclare */
+function incorrect() {
+  /*eslint array-element-newline: ["error", "consistent"]*/
+
+  var a = [
+    1, 2,
+    3
+  ];
+  var b = [
+    function foo() {
+      dosomething();
+    }, function bar() {
+      dosomething();
+    },
+    function baz() {
+      dosomething();
+    }
+  ];
+}
+
+function correct() {
+  /*eslint array-element-newline: ["error", "consistent"]*/
+
+  var a = [];
+  var b = [1];
+  var c = [1, 2];
+  var d = [1, 2, 3];
+  var e = [
+    1,
+    2
+  ];
+  var f = [
+    1,
+    2,
+    3
+  ];
+  var g = [
+    function foo() {
+      dosomething();
+    }, function bar() {
+      dosomething();
+    }, function baz() {
+      dosomething();
+    }
+  ];
+  var h = [
+    function foo() {
+      dosomething();
+    },
+    function bar() {
+      dosomething();
+    },
+    function baz() {
+      dosomething();
+      }
+  ];
+}
