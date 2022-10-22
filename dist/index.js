7536c8f382b4b809d6c947a049b24d2fdc552ae7
Cat Chen <catchen@catchen.me>
2022-10-22 16:28:09 -0700

    Separated the getESLint function to its own module (#404)

--- dist/index.js
+++ dist/index.js
@@ -11253,7 +11253,7 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
-/***/ 6144:
+/***/ 5173:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
@@ -11270,27 +11270,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.run = exports.defaultEventHandler = exports.pushEventHandler = exports.getPushFiles = exports.getPushMetadata = exports.pullRequestEventHandler = exports.matchReviewComments = exports.getCommentFromFix = exports.getIndexedModifiedLines = exports.getReviewThreads = exports.getReviewComments = exports.getPullRequestFiles = exports.getPullRequestMetadata = exports.getOctokit = exports.getESLintOutput = exports.getESLint = exports.changeDirectory = void 0;
-const github_1 = __nccwpck_require__(5438);
-const utils_1 = __nccwpck_require__(3030);
-const core_1 = __nccwpck_require__(2186);
-const plugin_throttling_1 = __nccwpck_require__(9968);
-const plugin_retry_1 = __nccwpck_require__(6298);
-const node_process_1 = __importDefault(__nccwpck_require__(7742));
-const node_child_process_1 = __importDefault(__nccwpck_require__(7718));
-const node_path_1 = __importDefault(__nccwpck_require__(9411));
-const node_fs_1 = __nccwpck_require__(7561);
+exports.getESLint = void 0;
 const module_1 = __nccwpck_require__(8188);
-const HUNK_HEADER_PATTERN = /^@@ -\d+(,\d+)? \+(\d+)(,(\d+))? @@/;
-const WORKING_DIRECTORY = node_process_1.default.cwd();
-const REVIEW_BODY = "ESLint doesn't pass. Please fix all ESLint issues.";
-function changeDirectory() {
-    (0, core_1.info)(`Working directory is: ${WORKING_DIRECTORY}`);
-    const absoluteDirectory = node_path_1.default.resolve(WORKING_DIRECTORY, (0, core_1.getInput)("directory"));
-    (0, core_1.info)(`Working directory is changed to: ${absoluteDirectory}`);
-    node_process_1.default.chdir(absoluteDirectory);
-}
-exports.changeDirectory = changeDirectory;
+const node_fs_1 = __nccwpck_require__(7561);
+const node_path_1 = __importDefault(__nccwpck_require__(9411));
+const core_1 = __nccwpck_require__(2186);
+const WORKING_DIRECTORY = process.cwd();
 function getESLint() {
     return __awaiter(this, void 0, void 0, function* () {
         const absoluteDirectory = node_path_1.default.resolve(WORKING_DIRECTORY, (0, core_1.getInput)("directory"));
@@ -11312,6 +11297,47 @@ function getESLint() {
     });
 }
 exports.getESLint = getESLint;
+
+
+/***/ }),
+
+/***/ 6144:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = exports.defaultEventHandler = exports.pushEventHandler = exports.getPushFiles = exports.getPushMetadata = exports.pullRequestEventHandler = exports.matchReviewComments = exports.getCommentFromFix = exports.getIndexedModifiedLines = exports.getReviewThreads = exports.getReviewComments = exports.getPullRequestFiles = exports.getPullRequestMetadata = exports.getOctokit = exports.getESLintOutput = exports.changeDirectory = void 0;
+const github_1 = __nccwpck_require__(5438);
+const utils_1 = __nccwpck_require__(3030);
+const core_1 = __nccwpck_require__(2186);
+const plugin_throttling_1 = __nccwpck_require__(9968);
+const plugin_retry_1 = __nccwpck_require__(6298);
+const node_process_1 = __importDefault(__nccwpck_require__(7742));
+const node_child_process_1 = __importDefault(__nccwpck_require__(7718));
+const node_path_1 = __importDefault(__nccwpck_require__(9411));
+const getESLint_1 = __nccwpck_require__(5173);
+const HUNK_HEADER_PATTERN = /^@@ -\d+(,\d+)? \+(\d+)(,(\d+))? @@/;
+const WORKING_DIRECTORY = node_process_1.default.cwd();
+const REVIEW_BODY = "ESLint doesn't pass. Please fix all ESLint issues.";
+function changeDirectory() {
+    (0, core_1.info)(`Working directory is: ${WORKING_DIRECTORY}`);
+    const absoluteDirectory = node_path_1.default.resolve(WORKING_DIRECTORY, (0, core_1.getInput)("directory"));
+    (0, core_1.info)(`Working directory is changed to: ${absoluteDirectory}`);
+    node_process_1.default.chdir(absoluteDirectory);
+}
+exports.changeDirectory = changeDirectory;
 function getESLintOutput(eslintBinPath) {
     return __awaiter(this, void 0, void 0, function* () {
         const targets = (0, core_1.getInput)("targets");
@@ -11901,7 +11927,7 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         (0, core_1.startGroup)("ESLint");
         changeDirectory();
-        const { eslint, eslintBinPath } = yield getESLint();
+        const { eslint, eslintBinPath } = yield (0, getESLint_1.getESLint)();
         const results = yield getESLintOutput(eslintBinPath);
         const indexedResults = {};
         for (const file of results) {
