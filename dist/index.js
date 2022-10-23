10a4d63471296c061c3043b5f2c60ad5bf8e9d7e
Cat Chen <catchen@catchen.me>
2022-10-22 22:10:03 -0700

    Separated the defaultEventHandler function and dependencies to its own module (#412)

--- dist/index.js
+++ dist/index.js
@@ -11253,6 +11253,87 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 5635:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.defaultEventHandler = void 0;
+const node_path_1 = __importDefault(__nccwpck_require__(9411));
+const core_1 = __nccwpck_require__(2186);
+const WORKING_DIRECTORY = process.cwd();
+function defaultEventHandler(eventName, results, ruleMetaDatas) {
+    var _a, _b, _c;
+    return __awaiter(this, void 0, void 0, function* () {
+        const failCheck = (0, core_1.getBooleanInput)("fail-check");
+        (0, core_1.startGroup)(`GitHub ${eventName}`);
+        let warningCounter = 0;
+        let errorCounter = 0;
+        for (const result of results) {
+            const relativePath = node_path_1.default.relative(WORKING_DIRECTORY, result.filePath);
+            for (const message of result.messages) {
+                if (message.ruleId === null || result.source === undefined) {
+                    continue;
+                }
+                const rule = ruleMetaDatas[message.ruleId];
+                (0, core_1.info)(`  ${relativePath}:${message.line}`);
+                switch (message.severity) {
+                    case 0:
+                        (0, core_1.notice)(`[${message.ruleId}]${message.message}: (${(_a = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _a === void 0 ? void 0 : _a.url})`, {
+                            file: relativePath,
+                            startLine: message.line,
+                        });
+                        break;
+                    case 1:
+                        (0, core_1.warning)(`[${message.ruleId}]${message.message}: (${(_b = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _b === void 0 ? void 0 : _b.url})`, {
+                            file: relativePath,
+                            startLine: message.line,
+                        });
+                        warningCounter++;
+                        break;
+                    case 2:
+                        (0, core_1.error)(`[${message.ruleId}]${message.message}: (${(_c = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _c === void 0 ? void 0 : _c.url})`, {
+                            file: relativePath,
+                            startLine: message.line,
+                        });
+                        errorCounter++;
+                        break;
+                }
+            }
+        }
+        (0, core_1.endGroup)();
+        (0, core_1.startGroup)("Feedback");
+        if (warningCounter > 0 || errorCounter > 0) {
+            if (failCheck) {
+                throw new Error("ESLint fails.");
+            }
+            else {
+                (0, core_1.error)("ESLint fails");
+            }
+        }
+        else {
+            (0, core_1.info)("ESLint passes");
+        }
+        (0, core_1.endGroup)();
+    });
+}
+exports.defaultEventHandler = defaultEventHandler;
+
+
+/***/ }),
+
 /***/ 5173:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -11541,15 +11622,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.run = exports.defaultEventHandler = exports.changeDirectory = void 0;
-const github_1 = __nccwpck_require__(5438);
-const core_1 = __nccwpck_require__(2186);
+exports.run = exports.changeDirectory = void 0;
 const node_process_1 = __importDefault(__nccwpck_require__(7742));
 const node_path_1 = __importDefault(__nccwpck_require__(9411));
+const github_1 = __nccwpck_require__(5438);
+const core_1 = __nccwpck_require__(2186);
 const getESLint_1 = __nccwpck_require__(5173);
 const getESLintOutput_1 = __nccwpck_require__(2580);
 const pullRequestEventHandler_1 = __nccwpck_require__(8344);
 const pushEventHandler_1 = __nccwpck_require__(5570);
+const defaultEventHandler_1 = __nccwpck_require__(5635);
 const WORKING_DIRECTORY = node_process_1.default.cwd();
 function changeDirectory() {
     (0, core_1.info)(`Working directory is: ${WORKING_DIRECTORY}`);
@@ -11558,62 +11640,6 @@ function changeDirectory() {
     node_process_1.default.chdir(absoluteDirectory);
 }
 exports.changeDirectory = changeDirectory;
-function defaultEventHandler(eventName, results, ruleMetaDatas) {
-    var _a, _b, _c;
-    return __awaiter(this, void 0, void 0, function* () {
-        const failCheck = (0, core_1.getBooleanInput)("fail-check");
-        (0, core_1.startGroup)(`GitHub ${eventName}`);
-        let warningCounter = 0;
-        let errorCounter = 0;
-        for (const result of results) {
-            const relativePath = node_path_1.default.relative(WORKING_DIRECTORY, result.filePath);
-            for (const message of result.messages) {
-                if (message.ruleId === null || result.source === undefined) {
-                    continue;
-                }
-                const rule = ruleMetaDatas[message.ruleId];
-                (0, core_1.info)(`  ${relativePath}:${message.line}`);
-                switch (message.severity) {
-                    case 0:
-                        (0, core_1.notice)(`[${message.ruleId}]${message.message}: (${(_a = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _a === void 0 ? void 0 : _a.url})`, {
-                            file: relativePath,
-                            startLine: message.line,
-                        });
-                        break;
-                    case 1:
-                        (0, core_1.warning)(`[${message.ruleId}]${message.message}: (${(_b = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _b === void 0 ? void 0 : _b.url})`, {
-                            file: relativePath,
-                            startLine: message.line,
-                        });
-                        warningCounter++;
-                        break;
-                    case 2:
-                        (0, core_1.error)(`[${message.ruleId}]${message.message}: (${(_c = rule === null || rule === void 0 ? void 0 : rule.docs) === null || _c === void 0 ? void 0 : _c.url})`, {
-                            file: relativePath,
-                            startLine: message.line,
-                        });
-                        errorCounter++;
-                        break;
-                }
-            }
-        }
-        (0, core_1.endGroup)();
-        (0, core_1.startGroup)("Feedback");
-        if (warningCounter > 0 || errorCounter > 0) {
-            if (failCheck) {
-                throw new Error("ESLint fails.");
-            }
-            else {
-                (0, core_1.error)("ESLint fails");
-            }
-        }
-        else {
-            (0, core_1.info)("ESLint passes");
-        }
-        (0, core_1.endGroup)();
-    });
-}
-exports.defaultEventHandler = defaultEventHandler;
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         (0, core_1.startGroup)("ESLint");
@@ -11662,14 +11688,14 @@ function run() {
                                     .split("_")
                                     .map((word) => { var _a; return ((_a = word[0]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) + word.substring(1); })
                                     .join(" ");
-                                defaultEventHandler(`Workflow (${workflowSourceEventName})`, results, ruleMetaDatas);
+                                (0, defaultEventHandler_1.defaultEventHandler)(`Workflow (${workflowSourceEventName})`, results, ruleMetaDatas);
                             })();
                             break;
                     }
                 })();
                 break;
             default:
-                defaultEventHandler(github_1.context.eventName
+                (0, defaultEventHandler_1.defaultEventHandler)(github_1.context.eventName
                     .split("_")
                     .map((word) => { var _a; return ((_a = word[0]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) + word.substring(1); })
                     .join(" "), results, ruleMetaDatas);
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,2 @@
-declare type LintResult = import("eslint").ESLint.LintResult;
-declare type RuleMetaData = import("eslint").Rule.RuleMetaData;
 export declare function changeDirectory(): void;
-export declare function defaultEventHandler(eventName: string, results: LintResult[], ruleMetaDatas: {
-    [name: string]: RuleMetaData;
-}): Promise<void>;
 export declare function run(): Promise<void>;
-export {};
