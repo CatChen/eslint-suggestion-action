6f6bf455d493495bfed12e5992fe697560c438c1
Cat Chen <catchen@catchen.me>
2022-10-22 20:34:55 -0700

    Separated the pushEventHandler function and dependencies to its own module (#411)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,26 +1,6 @@
-import { Octokit } from "@octokit/core";
-import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 declare type LintResult = import("eslint").ESLint.LintResult;
 declare type RuleMetaData = import("eslint").Rule.RuleMetaData;
 export declare function changeDirectory(): void;
-export declare function getPushFiles(owner: string, repo: string, beforeSha: string, afterSha: string, octokit: Octokit & Api): Promise<{
-    sha: string;
-    filename: string;
-    status: "added" | "removed" | "modified" | "renamed" | "copied" | "changed" | "unchanged";
-    additions: number;
-    deletions: number;
-    changes: number;
-    blob_url: string;
-    raw_url: string;
-    contents_url: string;
-    patch?: string | undefined;
-    previous_filename?: string | undefined;
-}[] | undefined>;
-export declare function pushEventHandler(indexedResults: {
-    [file: string]: LintResult;
-}, ruleMetaDatas: {
-    [name: string]: RuleMetaData;
-}): Promise<void>;
 export declare function defaultEventHandler(eventName: string, results: LintResult[], ruleMetaDatas: {
     [name: string]: RuleMetaData;
 }): Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,10 @@ import {
 import { WorkflowRunEvent } from "@octokit/webhooks-definitions/schema";
 import process from "node:process";
 import path from "node:path";
-import { Octokit } from "@octokit/core";
-import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 import { getESLint } from "./getESLint";
 import { getESLintOutput } from "./getESLintOutput";
-import { getOctokit } from "./getOctokit";
-import { getPushMetadata } from "./getPushMetadata";
-import { getIndexedModifiedLines } from "./getIndexedModifiedLines";
 import { pullRequestEventHandler } from "./pullRequestEventHandler";
+import { pushEventHandler } from "./pushEventHandler";
 
 type LintResult = import("eslint").ESLint.LintResult;
 type RuleMetaData = import("eslint").Rule.RuleMetaData;
@@ -34,112 +30,6 @@ export function changeDirectory() {
   );
   info(`Working directory is changed to: ${absoluteDirectory}`);
   process.chdir(absoluteDirectory);
-}
-
-export async function getPushFiles(
-  owner: string,
-  repo: string,
-  beforeSha: string,
-  afterSha: string,
-  octokit: Octokit & Api
-) {
-  const response = await octokit.rest.repos.compareCommitsWithBasehead({
-    owner,
-    repo,
-    basehead: `${beforeSha}...${afterSha}`,
-  });
-  info(`Files: (${response.data.files?.length ?? 0})`);
-  return response.data.files;
-}
-
-export async function pushEventHandler(
-  indexedResults: {
-    [file: string]: LintResult;
-  },
-  ruleMetaDatas: {
-    [name: string]: RuleMetaData;
-  }
-) {
-  const failCheck = getBooleanInput("fail-check");
-
-  startGroup("GitHub Push");
-  const octokit = getOctokit();
-  const { owner, repo, beforeSha, afterSha } = await getPushMetadata();
-  const files = await getPushFiles(owner, repo, beforeSha, afterSha, octokit);
-
-  if (files === undefined || files.length === 0) {
-    info(`Push contains no files`);
-    return;
-  }
-
-  let warningCounter = 0;
-  let errorCounter = 0;
-  for (const file of files) {
-    info(`  File name: ${file.filename}`);
-    info(`  File status: ${file.status}`);
-    if (file.status === "removed") {
-      continue;
-    }
-
-    const indexedModifiedLines = getIndexedModifiedLines(file);
-
-    const result = indexedResults[file.filename];
-    if (result) {
-      for (const message of result.messages) {
-        if (message.ruleId === null || result.source === undefined) {
-          continue;
-        }
-        const rule = ruleMetaDatas[message.ruleId];
-        if (indexedModifiedLines[message.line]) {
-          info(`  Matched line: ${message.line}`);
-          switch (message.severity) {
-            case 0:
-              notice(
-                `[${message.ruleId}]${message.message}: (${rule?.docs?.url})`,
-                {
-                  file: file.filename,
-                  startLine: message.line,
-                }
-              );
-              break;
-            case 1:
-              warning(
-                `[${message.ruleId}]${message.message}: (${rule?.docs?.url})`,
-                {
-                  file: file.filename,
-                  startLine: message.line,
-                }
-              );
-              warningCounter++;
-              break;
-            case 2:
-              error(
-                `[${message.ruleId}]${message.message}: (${rule?.docs?.url})`,
-                {
-                  file: file.filename,
-                  startLine: message.line,
-                }
-              );
-              errorCounter++;
-              break;
-          }
-        }
-      }
-    }
-  }
-  endGroup();
-
-  startGroup("Feedback");
-  if (warningCounter > 0 || errorCounter > 0) {
-    if (failCheck) {
-      throw new Error("ESLint fails. Please review comments.");
-    } else {
-      error("ESLint fails");
-    }
-  } else {
-    info("ESLint passes");
-  }
-  endGroup();
 }
 
 export async function defaultEventHandler(
