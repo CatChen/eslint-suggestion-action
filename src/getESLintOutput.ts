b3b37a258f9442dff5760025b6f5533b4d8740b8
Cat Chen <catchen@catchen.me>
2022-12-21 23:09:58 -0800

    Switched to @actions/glob for targets handling

--- src/getESLintOutput.ts
+++ src/getESLintOutput.ts
@@ -1,18 +1,23 @@
-import childProcess from "node:child_process";
-import { getInput } from "@actions/core";
+import { join } from "node:path";
+import { ExitCode, getInput } from "@actions/core";
+import { getExecOutput } from "@actions/exec";
+import { create } from "@actions/glob";
 
 import type { ESLint } from "eslint";
 
 export async function getESLintOutput(eslintBinPath: string) {
   const targets = getInput("targets");
-  let results: ESLint.LintResult[] = [];
-  try {
-    const stdout = childProcess.execSync(
-      `${eslintBinPath} "${targets}" --no-error-on-unmatched-pattern --format json`
-    );
-    results = JSON.parse(stdout.toString());
-  } catch (error) {
-    // Ignore the error.
+  const globber = await create(targets);
+  const glob = await globber.glob();
+  const eslintOutput = await getExecOutput(eslintBinPath, [
+    ...glob,
+    "--no-error-on-unmatched-pattern",
+    "--format",
+    "json",
+  ]);
+  if (eslintOutput.exitCode !== ExitCode.Success) {
+    throw new Error(eslintOutput.stderr);
   }
+  const results: ESLint.LintResult[] = JSON.parse(eslintOutput.stdout);
   return results;
 }
