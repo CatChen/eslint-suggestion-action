fbdd6254a3575235780efcfe6b2d2e6553986872
Cat Chen <catchen@catchen.me>
2024-05-04 16:33:53 -0700

    Compatibility fix for ESLint 8.x (#950)

--- src/getESLint.ts
+++ src/getESLint.ts
@@ -18,9 +18,33 @@ export async function getESLint() {
   notice(`Using ESLint from: ${eslintJsPath}`);
   const { ESLint, loadESLint } = require(eslintJsPath) as {
     ESLint: typeof ProjectESLint;
-    loadESLint: () => Promise<typeof ProjectESLint>;
+    loadESLint: (() => Promise<typeof ProjectESLint>) | undefined;
   };
   notice(`ESLint version: ${ESLint.version}`);
-  const eslint = new (await loadESLint())();
+
+  const configPath = getInput('config-path');
+  if (configPath) {
+    const absoluteConfigPath = resolve(absoluteDirectory, configPath);
+    notice(`Using ESLint config from: ${absoluteConfigPath}`);
+    const eslint = new ESLint({ overrideConfigFile: absoluteConfigPath });
+    return eslint;
+  }
+
+  if (loadESLint) {
+    // ESLint 8.57.0 and later
+    notice('Using ESLint with default configuration');
+    const eslint = new (await loadESLint())();
+    return eslint;
+  }
+
+  const eslintConfig = (await new ESLint().calculateConfigForFile(
+    'package.json',
+  )) as ProjectESLint.ConfigData | undefined;
+  if (!eslintConfig) {
+    throw new Error(
+      'Failed to find ESLint configuration. Please set the config-path input.',
+    );
+  }
+  const eslint = new ESLint({ baseConfig: eslintConfig });
   return eslint;
 }
