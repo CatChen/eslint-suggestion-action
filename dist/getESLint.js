d1dc327327e91f164763540d44e135816c369f67
Cat Chen <catchen@catchen.me>
2024-07-01 17:35:06 -0700

    Switched module.require to dynamic import

--- dist/getESLint.js
+++ dist/getESLint.js
@@ -1,17 +1,15 @@
-import { createRequire } from 'module';
 import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { getInput, notice } from '@actions/core';
 import { DEFAULT_WORKING_DIRECTORY } from './changeDirectory.js';
 export async function getESLint() {
     const absoluteDirectory = resolve(DEFAULT_WORKING_DIRECTORY, getInput('directory'));
-    const require = createRequire(absoluteDirectory);
     const eslintJsPath = resolve(absoluteDirectory, getInput('eslint-lib-path'));
     if (!existsSync(eslintJsPath)) {
         throw new Error(`ESLint JavaScript cannot be found at ${eslintJsPath}`);
     }
     notice(`Using ESLint from: ${eslintJsPath}`);
-    const { ESLint, loadESLint } = require(eslintJsPath);
+    const { ESLint, loadESLint } = (await import(resolve(absoluteDirectory, eslintJsPath)));
     notice(`ESLint version: ${ESLint.version}`);
     const configPath = getInput('config-path');
     if (configPath) {
