fb83a234cca648fb6b25d09b57e847d248ca5c8a
Cat Chen <catchen@catchen.me>
2024-07-07 22:23:51 -0700

    Not changing directory if not needed

--- dist/push.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Octokit } from '@octokit/core';
-import type { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types.js';
-import type { ESLint, Rule } from 'eslint';
-export declare function handlePush(octokit: Octokit & Api, indexedResults: {
-    [file: string]: ESLint.LintResult;
-}, ruleMetaDatas: {
-    [name: string]: Rule.RuleMetaData;
-}, owner: string, repo: string, beforeSha: string, afterSha: string): Promise<void>;
