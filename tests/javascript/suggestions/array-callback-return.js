5670ff682e3434a897c729cc62d494bf8c72a0ff
Cat Chen <catchen@catchen.me>
2024-07-15 11:54:00 -0700

    Merged variations of suggestion tests

--- tests/javascript/suggestions/array-callback-return.js
+++ tests/javascript/suggestions/array-callback-return.js
@@ -14,25 +14,103 @@ function incorrect() {
 
   var bar = foo.filter(function(x) {
     if (x) {
-        return true;
+      return true;
     } else {
-        return;
+      return;
     }
   });
 }
 
 function correct() {
   var indexMap = myArray.reduce(function(memo, item, index) {
-      memo[item] = index;
-      return memo;
+    memo[item] = index;
+    return memo;
   }, {});
 
   var foo = Array.from(nodes, function(node) {
-      if (node.tagName === "DIV") {
-          return true;
-      }
-      return false;
+    if (node.tagName === "DIV") {
+      return true;
+    }
+    return false;
   });
 
   var bar = foo.map(node => node.getAttribute("id"));
+}
+
+/*eslint array-callback-return: ["error", { allowImplicit: true }]*/
+function correct() {
+  var undefAllTheThings = myArray.map(function(item) {
+    return;
+  });
+}
+
+/*eslint array-callback-return: ["error", { checkForEach: true }]*/
+function incorrect() {
+  myArray.forEach(function(item) {
+      return handleItem(item);
+  });
+
+  myArray.forEach(function(item) {
+    if (item < 0) {
+      return x;
+    }
+    handleItem(item);
+  });
+
+  myArray.forEach(function(item) {
+    if (item < 0) {
+      return void x;
+    }
+    handleItem(item);
+  });
+
+  myArray.forEach(item => handleItem(item));
+
+  myArray.forEach(item => void handleItem(item));
+
+  myArray.forEach(item => {
+    return handleItem(item);
+  });
+
+  myArray.forEach(item => {
+    return void handleItem(item);
+  });
+}
+
+function correct() {
+  myArray.forEach(function(item) {
+    handleItem(item)
+  });
+
+  myArray.forEach(function(item) {
+    if (item < 0) {
+      return;
+    }
+    handleItem(item);
+  });
+
+  myArray.forEach(function(item) {
+    handleItem(item);
+    return;
+  });
+
+  myArray.forEach(item => {
+      handleItem(item);
+  });
+}
+
+/*eslint array-callback-return: ["error", { allowVoid: true }]*/
+function correct() {
+  myArray.forEach(item => void handleItem(item));
+
+  myArray.forEach(item => {
+    return void handleItem(item);
+  });
+
+  myArray.forEach(item => {
+    if (item < 0) {
+      return void x;
+    }
+    handleItem(item);
+  });
 }
