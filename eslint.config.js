b71cc409ac312fe1e00f836f29baa21fe5237209
Cat Chen <catchen@catchen.me>
2024-08-31 21:41:10 -0700

    Fixed ESLint typing (#1486)

--- eslint.config.js
+++ eslint.config.js
@@ -13,34 +13,46 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
 });
 
-export default ts.config(
-  ...compat.config({
-    env: {
-      browser: true,
-      es2022: true,
-      node: true,
+const jsConfigs = compat.config({
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  root: true,
+  rules: {},
+  ignorePatterns: [
+    'tests/**/*',
+    'node_modules/**/*',
+    'dist/**/*',
+    'bundle/**/*',
+  ],
+  overrides: [
+    {
+      files: ['**/*.js'],
     },
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    root: true,
-    rules: {},
-    ignorePatterns: [
-      'tests/**/*',
-      'node_modules/**/*',
-      'dist/**/*',
-      'bundle/**/*',
-      'eslint.config.js',
-    ],
-    overrides: [
-      {
-        files: ['*.ts'],
-      },
-    ],
-  }),
+  ],
+});
+
+const tsConfigs = [
   ...ts.configs.recommendedTypeChecked,
-);
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+].map((config) => ({
+  ...config,
+  files: ['src/**/*.ts'],
+}));
+
+const configs = ts.config(...jsConfigs, ...tsConfigs);
+
+export default configs;
