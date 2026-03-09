a71e9f4f48687883b839a43600f26ca8c391370a
Cat Chen <catchen@catchen.me>
2026-03-09 05:18:48 +0000

    Push test (>300 files) for a640d76f75faef8f7d2d54e34a8b23f639b788ca by run 22839506766 (5585-1)

---
+++ src/__tests__/bulk/file-275.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* eslint-enable no-array-constructor */
+/*eslint no-array-constructor: "error"*/
+Array(500);
+
+new Array(someOtherArray.length);
+
+[0, 1, 2];
+
+const createArray = Array => new Array();
