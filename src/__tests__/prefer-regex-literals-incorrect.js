d410d096c9810d11bd9067a2e9bcc6522c75d591
Cat Chen <catchen@catchen.me>
2026-03-16 05:42:40 +0000

    Test commit for #4733 (790c32d3d4f7725d7c07337101de2f1f915ef50d) by run 23129654455 (5797-1)

---
+++ src/__tests__/prefer-regex-literals-incorrect.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+/* eslint-enable prefer-regex-literals */
+/*eslint prefer-regex-literals: "error"*/
+new RegExp("abc");
+
+new RegExp("abc", "u");
+
+RegExp("abc");
+
+RegExp("abc", "u");
+
+new RegExp("\\d\\d\\.\\d\\d\\.\\d\\d\\d\\d");
+
+RegExp(`^\\d\\.$`);
+
+new RegExp(String.raw`^\d\.$`);
