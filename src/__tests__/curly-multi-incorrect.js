1603833f33b153ba4806d4f692c99bc95db11207
Cat Chen <catchen@catchen.me>
2026-03-23 19:55:36 +0000

    Test commit for #4800 (603a9afb57f01827f0b00038afd139fd5c0c6917) by run 23457153668 (5865-1)

---
+++ src/__tests__/curly-multi-incorrect.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/* eslint-enable curly */
+/*eslint curly: ["error", "multi"]*/
+if (foo) {
+  foo++;
+}
+
+if (foo) bar();
+else {
+  foo++;
+}
+
+while (true) {
+  doSomething();
+}
+
+for (var i=0; i < items.length; i++) {
+  doSomething();
+}
