8add16e651c13564e37772546a85aafd92815c46
Cat Chen <catchen@catchen.me>
2026-04-13 20:53:00 +0000

    Test commit for #4956 (be761aaf3f3fcf699077f3643bb3c86cba533469) by run 24366253060 (6016-1)

---
+++ src/__tests__/sort-imports-ignoreCaseTrue-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "ignoreCase": true }]*/
+// Incorrect Code
+import B from 'foo.js';
+import a from 'bar.js';
