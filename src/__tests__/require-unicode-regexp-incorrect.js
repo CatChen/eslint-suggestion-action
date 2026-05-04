63ac3abde10bdb5a3c5b0b07a3dd14e0099aa347
Cat Chen <catchen@catchen.me>
2026-05-04 23:55:52 +0000

    Test commit for #5058 (e35948b6ccddf7371e0f11d4569dc3cb67eb7af8) by run 25350103138 (6113-1)

---
+++ src/__tests__/require-unicode-regexp-incorrect.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+/* eslint-enable require-unicode-regexp */
+/*eslint require-unicode-regexp: "error"*/
+const a = /aaa/
+const b = /bbb/gi
+const c = new RegExp("ccc")
+const d = new RegExp("ddd", "gi")
