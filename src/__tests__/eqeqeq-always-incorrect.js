45da9c4a0ba9da2e8dac3429382fa0b4bb2432b4
Cat Chen <catchen@catchen.me>
2026-05-20 03:12:37 +0000

    Test commit for #5166 (e593111d8c8e83d67f882476bc8523fbfeaa5f9f) by run 26138925725 (6261-1)

---
+++ src/__tests__/eqeqeq-always-incorrect.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* eslint-enable eqeqeq */
+/*eslint eqeqeq: ["error", "always"]*/
+a == b
+foo == true
+bananas != 1
+value == undefined
+typeof foo == 'undefined'
+'hello' != 'world'
+0 == 0
+true == true
+foo == null
