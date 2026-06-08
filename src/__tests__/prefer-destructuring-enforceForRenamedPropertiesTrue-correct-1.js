bb495c0127cb64198ef945f5881a0e15d9d17188
Cat Chen <catchen@catchen.me>
2026-06-08 20:00:58 +0000

    Test commit for #5317 (ad73911cfd3512e2aaad49b5c63da6614aee0ada) by run 27163378801 (6360-1)

---
+++ src/__tests__/prefer-destructuring-enforceForRenamedPropertiesTrue-correct-1.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+/* eslint-enable prefer-destructuring */
+/* eslint prefer-destructuring: ["error", {"object": true, "array": true}, {"enforceForRenamedProperties": true}] */
+var { bar: foo } = object;
