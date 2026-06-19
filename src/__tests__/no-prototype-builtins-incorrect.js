d5bc0ed333e0969567798a5a5ee63caf4506fd49
Cat Chen <catchen@catchen.me>
2026-06-19 06:37:27 +0000

    Test commit for #5392 (8e6f4b8079ae89750569c63ff09adf29ba038203) by run 27809863758 (6473-1)

---
+++ src/__tests__/no-prototype-builtins-incorrect.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* eslint-enable no-prototype-builtins */
+/*eslint no-prototype-builtins: "error"*/
+var hasBarProperty = foo.hasOwnProperty("bar");
+
+var isPrototypeOfBar = foo.isPrototypeOf(bar);
+
+var barIsEnumerable = foo.propertyIsEnumerable("bar");
