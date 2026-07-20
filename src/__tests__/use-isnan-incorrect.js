d8b144492f703b87b930a76599087eba6e34a294
Cat Chen <catchen@catchen.me>
2026-07-20 19:55:28 +0000

    Test commit for #5650 (8152f53b615b8999c4afd984367dfa89e8f51ef7) by run 29773859330 (6670-1)

---
+++ src/__tests__/use-isnan-incorrect.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* eslint-enable use-isnan */
+/*eslint use-isnan: "error"*/
+if (foo == NaN) {
+    // ...
+}
+
+if (foo != NaN) {
+    // ...
+}
+
+if (foo == Number.NaN) {
+    // ...
+}
+
+if (foo != Number.NaN) {
+    // ...
+}
