63b7dea04e43ec929eaf76853f715c44db9154f3
Cat Chen <catchen@catchen.me>
2026-07-22 03:14:21 +0000

    Test commit for main (42daf287467e67e5f05f604a5f99136256324028) by run 29886638045 (6721-1)

---
+++ src/__tests__/logical-assignment-operators-incorrect.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* eslint-enable logical-assignment-operators */
+/*eslint logical-assignment-operators: ["error", "always"]*/
+a = a || b
+a = a && b
+a = a ?? b
+a || (a = b)
+a && (a = b)
+a ?? (a = b)
+a = a || b || c
+a = a && b && c
+a = a ?? b ?? c
