35dd96a172311b64e7b1ae1100a9dbd18500fb78
Cat Chen <catchen@catchen.me>
2026-08-01 02:35:46 +0000

    Test commit for #5823 (2a78293e019119f565d9317c0a3a4ade66a3dc4f) by run 30680316718 (6839-1)

---
+++ src/__tests__/prefer-regex-literals-disallowRedundantWrappingTrue-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable prefer-regex-literals */
+/*eslint prefer-regex-literals: ["error", {"disallowRedundantWrapping": true}]*/
+new RegExp(/abc/);
+
+new RegExp(/abc/, 'u');
