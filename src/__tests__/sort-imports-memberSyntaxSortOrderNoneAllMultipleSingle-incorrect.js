b048f1265f31e0480d105e27d8129119bc2913ea
Cat Chen <catchen@catchen.me>
2026-08-03 15:49:53 +0000

    Test commit for #5886 (05ea3d406201db7594a2d1806feeb35848250202) by run 30829308270 (6886-1)

---
+++ src/__tests__/sort-imports-memberSyntaxSortOrderNoneAllMultipleSingle-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "memberSyntaxSortOrder": ["none", "all", "multiple", "single"] }]*/
+// Incorrect Code
+import a from 'foo.js';
+import * as b from 'bar.js';
