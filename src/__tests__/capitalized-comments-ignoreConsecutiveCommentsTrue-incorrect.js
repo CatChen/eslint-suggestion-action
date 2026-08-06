d09e8b9be03295d3f2f5b23fff8a8fdf2974ad38
Cat Chen <catchen@catchen.me>
2026-08-06 13:41:14 +0000

    Test commit for #5999 (2a30cbdf61e9a92e56f70d7bb5ddf5694acdf724) by run 31107082680 (6972-1)

---
+++ src/__tests__/capitalized-comments-ignoreConsecutiveCommentsTrue-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable capitalized-comments */
+/* eslint capitalized-comments: ["error", "always", { "ignoreConsecutiveComments": true }] */
+foo();
+// this comment is invalid, but only on this line.
+// this comment does NOT get reported, since it is a consecutive comment.
