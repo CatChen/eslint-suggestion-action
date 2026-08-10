27b91d33ea1f65433ca3859189abaa6943bc10e0
Cat Chen <catchen@catchen.me>
2026-08-10 20:01:45 +0000

    Test commit for #6055 (d2678b036daaa7df4de134ae9b32f44d35691c28) by run 31426873645 (7031-1)

---
+++ src/__tests__/sort-imports-ignoreMemberSortTrue-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "ignoreMemberSort": true }]*/
+// Incorrect Code
+import b from 'foo.js';
+import a from 'bar.js';
