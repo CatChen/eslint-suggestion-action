7383625a2917b8154b31d9583f477ac1d8e5d425
Cat Chen <catchen@catchen.me>
2026-08-11 19:56:29 +0000

    Test commit for #6075 (23d8a4ad2e45a509944a105b14646560620f038f) by run 31530264206 (7043-1)

---
+++ src/__tests__/prefer-exponentiation-operator-incorrect.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* eslint-enable prefer-exponentiation-operator */
+/*eslint prefer-exponentiation-operator: "error"*/
+const foo = Math.pow(2, 8);
+
+const bar = Math.pow(a, b);
+
+let baz = Math.pow(a + b, c + d);
+
+let quux = Math.pow(-1, n);
