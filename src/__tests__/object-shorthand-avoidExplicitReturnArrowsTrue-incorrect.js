8ed186e7d1171e373ec166d382813071385e4595
Cat Chen <catchen@catchen.me>
2026-09-01 18:51:04 +0000

    Test commit for #6352 (426637626fba6a2c347fa813da6aa0070c944810) by run 33545094626 (7305-1)

---
+++ src/__tests__/object-shorthand-avoidExplicitReturnArrowsTrue-incorrect.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* eslint-enable object-shorthand */
+/*eslint object-shorthand: ["error", "always", { "avoidExplicitReturnArrows": true }]*/
+var foo = {
+  foo: (bar, baz) => {
+    return bar + baz;
+  },
+
+  qux: (foobar) => {
+    return foobar * 2;
+  }
+};
