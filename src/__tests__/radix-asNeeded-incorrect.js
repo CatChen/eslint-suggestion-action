cde8435a7ab086c1294b52572381fd4202946156
Cat Chen <catchen@catchen.me>
2026-09-03 03:28:52 +0000

    Test commit for main (04b01dbd0dde03178e5f2980e763bdb016ea2a21) by run 33711471462 (7321-1)

---
+++ src/__tests__/radix-asNeeded-incorrect.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* eslint-enable radix */
+/*eslint radix: ["error", "as-needed"]*/
+var num = parseInt("071", 10);
+
+var num = parseInt("071", "abc");
+
+var num = parseInt();
