6cf775fce043c6de2d1353ebd2177dddd9d85792
Cat Chen <catchen@catchen.me>
2026-09-06 16:51:21 +0000

    Test commit for #6478 (6d8896a091efd6a2063166dd9a8b15d09eeb1981) by run 34046767081 (7399-1)

---
+++ src/__tests__/eqeqeq-smart-incorrect.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* eslint-enable eqeqeq */
+/*eslint eqeqeq: ["error", "smart"]*/
+// comparing two variables requires ===
+a == b
+
+// only one side is a literal
+foo == true
+bananas != 1
+
+// comparing to undefined requires ===
+value == undefined
