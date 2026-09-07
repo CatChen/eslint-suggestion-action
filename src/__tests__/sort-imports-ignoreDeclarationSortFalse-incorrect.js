63e1988136d6d906c5d9d478e2bc1e9321c56596
Cat Chen <catchen@catchen.me>
2026-09-07 19:59:29 +0000

    Test commit for main (49ac2ed007182991cafb213b0b7759518d576134) by run 34157576596 (7426-1)

---
+++ src/__tests__/sort-imports-ignoreDeclarationSortFalse-incorrect.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "ignoreDeclarationSort": false }]*/
+// Incorrect Code
+import b from 'foo.js'
+import a from 'bar.js'
