016fcffb007da7266673e3c20c29de722122dbd7
Cat Chen <catchen@catchen.me>
2026-09-07 23:28:33 +0000

    Test commit for main (0ac091dbe5dca31856a2ce49c823c15f9703e6a1) by run 34170152156 (7431-1)

---
+++ src/__tests__/valid-typeof-incorrect.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+/* eslint-enable valid-typeof */
+/*eslint valid-typeof: "error"*/
+typeof foo === "strnig"
+typeof foo == "undefimed"
+typeof bar != "nunber"
+typeof bar !== "fucntion"
