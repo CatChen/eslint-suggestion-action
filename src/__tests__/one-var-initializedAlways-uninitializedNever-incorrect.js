25aa706cf11d4cb350aa375c4cf2ce84337fce5d
Cat Chen <catchen@catchen.me>
2026-09-08 04:16:52 +0000

    Test commit for #6521 (57c15188983125f1a71f00680154a1781856178f) by run 34186427780 (7435-1)

---
+++ src/__tests__/one-var-initializedAlways-uninitializedNever-incorrect.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* eslint-enable one-var */
+/*eslint one-var: ["error", { "initialized": "always", "uninitialized": "never" }]*/
+function foo() {
+  var a, b, c;
+  var foo = true;
+  var bar = false;
+}
