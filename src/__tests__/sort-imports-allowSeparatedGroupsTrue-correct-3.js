86debac6f8b580d71800649583629eacbe4accc7
Cat Chen <catchen@catchen.me>
2026-09-08 19:58:13 +0000

    Test commit for main (0acbcd849c64ef78c8a4a98976aaa9d85f2961a5) by run 34271908072 (7443-1)

---
+++ src/__tests__/sort-imports-allowSeparatedGroupsTrue-correct-3.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "allowSeparatedGroups": true }]*/
+// Correct Code
+import b from 'foo.js';
+import c from 'bar.js';
+quux();
+import a from 'baz.js';
