6a7b2d43b4f245e478e3c25637ee513eb08f25ac
Cat Chen <catchen@catchen.me>
2026-09-10 22:32:55 +0000

    Test commit for main (718c9c5f1a58a0c5e6ea416cf26dae9ba60a7e8c) by run 34537964231 (7475-1)

---
+++ src/__tests__/sort-imports-allowSeparatedGroupsTrue-incorrect.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+/* eslint-enable sort-imports */
+/*eslint sort-imports: ["error", { "allowSeparatedGroups": true }]*/
+// Incorrect Code
+import b from 'foo.js';
+import c from 'bar.js';
+import a from 'baz.js';
