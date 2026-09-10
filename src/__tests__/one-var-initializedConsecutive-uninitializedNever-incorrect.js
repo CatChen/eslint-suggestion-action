e202c86e1e2858813350f7bc10ebeba7e1c46b55
Cat Chen <catchen@catchen.me>
2026-09-10 19:54:57 +0000

    Test commit for #6544 (79581a23e04e0b96cfc64542cc018a1ddc5fdeab) by run 34523193234 (7456-1)

---
+++ src/__tests__/one-var-initializedConsecutive-uninitializedNever-incorrect.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* eslint-enable one-var */
+/*eslint one-var: ["error", { "initialized": "consecutive", "uninitialized": "never" }]*/
+function foo() {
+  var a = 1;
+  var b = 2;
+  var c,
+      d;
+  var e = 3;
+  var f = 4;
+}
