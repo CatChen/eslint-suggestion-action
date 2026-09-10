e7eb5d9af3bd77bb40f99e6bf41341ccdefa98d9
Cat Chen <catchen@catchen.me>
2026-09-10 20:04:14 +0000

    Test commit for #6558 (21c9bb24290a5b26e348190ca239cd2ec110fff7) by run 34524111861 (7470-1)

---
+++ src/__tests__/no-empty-incorrect.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/* eslint-enable no-empty */
+/*eslint no-empty: "error"*/
+if (foo) {
+}
+
+while (foo) {
+}
+
+switch(foo) {
+}
+
+try {
+  doSomething();
+} catch(ex) {
+
+} finally {
+
+}
