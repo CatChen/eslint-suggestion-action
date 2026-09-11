8a238d57b55b69c49160894c0dab47c414bbd071
Cat Chen <catchen@catchen.me>
2026-09-11 07:06:05 +0000

    Test commit for main (f3f8ef88f5e871ea54797a1f5a5c66791bd55934) by run 34572810484 (7479-1)

---
+++ src/__tests__/prefer-destructuring-incorrect.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* eslint-enable prefer-destructuring */
+/* eslint prefer-destructuring: ["error"] */
+// With `array` enabled
+var foo = array[0];
+
+// With `object` enabled
+var foo = object.foo;
+var foo = object['foo'];
