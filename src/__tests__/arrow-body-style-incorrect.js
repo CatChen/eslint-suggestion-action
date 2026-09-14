50daf253265010dca876f55d8a5adaac98759660
Cat Chen <catchen@catchen.me>
2026-09-14 19:55:03 +0000

    Test commit for #6583 (edfbed996ff3bc88d2f3a10cf62470abe83dff40) by run 34889749712 (7491-1)

---
+++ src/__tests__/arrow-body-style-incorrect.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* eslint-enable arrow-body-style */
+/*eslint arrow-body-style: ["error", "as-needed"]*/
+let foo = () => {
+  return 0;
+};
+let foo = () => {
+  return {
+    bar: {
+      foo: 1,
+      bar: 2,
+      }
+  };
+};
