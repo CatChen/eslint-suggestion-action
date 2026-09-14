cf72983a382449e1fef447b4252526e0415b6126
Cat Chen <catchen@catchen.me>
2026-09-14 19:55:36 +0000

    Test commit for #6580 (70f39d46d3735829450a06bd556d9608b966f669) by run 34889615886 (7488-1)

---
+++ src/__tests__/no-extra-label-incorrect.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* eslint-enable no-extra-label */
+/*eslint no-extra-label: "error"*/
+A: while (a) {
+  break A;
+}
+
+B: for (let i = 0; i < 10; ++i) {
+  break B;
+}
+
+C: switch (a) {
+  case 0:
+      break C;
+}
