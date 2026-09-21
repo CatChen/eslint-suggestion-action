40bdf20de2c1672ccc395d5d66252f9f54418f36
Cat Chen <catchen@catchen.me>
2026-09-21 19:58:19 +0000

    Test commit for main (2e8079e6ebe8f7c80872c7ad5bb045f04199675a) by run 35647978399 (7530-1)

---
+++ src/__tests__/no-useless-escape-incorrect.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* eslint-enable no-useless-escape */
+/*eslint no-useless-escape: "error"*/
+"\'";
+'\"';
+"\#";
+"\e";
+`\"`;
+`\"${foo}\"`;
+`\#{foo}`;
+/\!/;
+/\@/;
+/[\[]/;
+/[a-z\-]/;
